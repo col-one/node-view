19ac58bd20f26662e23a607ece34c0dce7fe2f12
col-one <col>
2016-12-06 15:05:49 +0100

    adjust qtflag , StringAttr

--- python/attributes.py
+++ python/attributes.py
@@ -3,19 +3,31 @@
 
 
 class AbstractAttr(object):
+    _value = None
+    is_dirty = False
+
     def __init__(self, value=None, implementation=None):
-        self._value = value
-        self.is_dirty = False
+        if not value is None:
+            self.value = value
 
     @property
     def value(self):
         return self._value
+
     @value.setter
     def value(self, v):
-        if self._value == v:
-            self.is_dirty = False
-        else:
-            self._value = v
-            self.is_dirty = True
+        if self.sanity(v):
+            if self._value == v:
+                self.is_dirty = False
+            else:
+                self._value = v
+                self.is_dirty = True
 
+    def sanity(self, v):
+        return True
 
+class StringAttr(AbstractAttr):
+    def sanity(self, v):
+        if not isinstance(v, str):
+            raise TypeError("Wrong value type, must be str")
+        return True
